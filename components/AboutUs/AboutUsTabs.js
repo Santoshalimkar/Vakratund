cdf4a8bc641e38df7d333c5bd745e3fdfa8c648c
Sandeep Kadam <sandeepkadam1798.sk@gmail.com>
2024-07-31 13:33:32 +0530

    changes

--- components/AboutUs/AboutUsTabs.js
+++ components/AboutUs/AboutUsTabs.js
@@ -42,10 +42,7 @@ export default function AboutUsTabs() {
                     <div>
                         {/* <h2 className="font-bold text-lg mb-2">Authentic Travel Experiences</h2> */}
                         <p>
-                    {`Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
-                    eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
-                    enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui 
-                    ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.`}
+                    {`Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia similique est fuga ratione explicabo excepturi, voluptas necessitatibus libero. Enim quas ullam sequi ex at ipsum rerum ipsa, quia quibusdam dicta?`}
                         </p>
                     </div>
                 )}
