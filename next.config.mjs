97572193904143723468c644f60dd5f61a2164ce
Santoshalimkar <santosh.sa4d@gmail.com>
2024-08-06 19:33:35 +0530

    Changes added

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'scontent.cdninstagram.com',
-        port: '',
-        pathname: '',
-      },
-    ],
-  },};
+const nextConfig = {};
 
 
 export default nextConfig;
