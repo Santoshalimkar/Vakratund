85da8d1b6a32878f28d29151e0f0d93463c5b9f2
Santoshalimkar <santosh.sa4d@gmail.com>
2024-08-06 21:25:43 +0530

    Changes added

--- app/page.js
+++ app/page.js
@@ -8,11 +8,19 @@ import Aboutus from "@/components/AboutUs/Aboutus";
 import ContactFormMain from "@/components/Contactuscomponents/ContactFormMain";
 import WeekendTripMain from "./destination/weekendtrip/page";
 import { Divider } from "@nextui-org/react";
+import Whats from "../public/Navabar/whats.png"
+import Link from "next/link";
            
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full ">
+    <div className="fixed bottom-12 right-8 z-50">
+    <Link href={"https://wa.link/f5qsnc"}>
+      <Image className="md:w-24 md:h-24 lg:w-24 lg:h-24 h-14 w-14 object-contain" src={Whats} alt="whatsapp"/>
+
+    </Link>
+    </div>
      <Videocomponent/>
      <Crouselslide/>
      {/* <Cardcrousel/> */}
