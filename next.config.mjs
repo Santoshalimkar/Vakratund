02e4be69d3480d7c4be80ef576c7ac5503f0036f
Sandeep Kadam <sandeepkadam1798.sk@gmail.com>
2024-08-12 00:55:16 +0530

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -8,15 +8,7 @@ const nextConfig = { images: {
       },
     ],
   },};
-const nextConfig = { images: {
-    domains: ["res.cloudinary.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-      },
-    ],
-  },};
+
 
 
   export default nextConfig;
