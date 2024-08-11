25a2adfe04382e493cabe0a9b3a93f93c5a8fe20
Santoshalimkar <santosh.sa4d@gmail.com>
2024-08-12 01:12:13 +0530

    changes added

--- app/page.js
+++ app/page.js
@@ -14,6 +14,7 @@ import RomanticEscape from "./honeymoon-packages/RomanticEscape";
 
 import UpcomingTripsCrousel from "./upcomingtrip/UpcomingTripsCrousel";
 import Reviewcrousel from "@/components/Crouselcomponents/Reviewcrousel";
+import Swipper from "@/components/Crouselcomponents/Swipper";
            
 
 export default function Home() {
@@ -37,6 +38,7 @@ export default function Home() {
      <Aboutus/>
      <Divider className="w-11/12 mx-auto mt-12 "/>
      <Reviewcrousel/>
+     <Swipper/>
      <ContactFormMain/>
 
     </main>
