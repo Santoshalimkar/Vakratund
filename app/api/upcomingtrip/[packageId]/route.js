db5b8da6a50b5e6d30e5fcebe0ccc4ce0f1220bd
Sandeep Kadam <sandeepkadam1798.sk@gmail.com>
2024-08-21 17:05:49 +0530

    working on api

---
+++ app/api/upcomingtrip/[packageId]/route.js
@@ -0,0 +1,27 @@
+// app/api/packages/[packageId]/route.js
+import dbConnect from '@/lib/Dbconnection/Dbconnect';
+import UpcommingTrips from '../../../../lib/model/UpcommingTripsschema';
+import { NextResponse } from 'next/server';
+import mongoose from 'mongoose';
+
+export async function GET(req,  { params }) {
+  await dbConnect();
+
+  const { packageId } = params;
+  console.log(packageId)
+
+  if (!mongoose.Types.ObjectId.isValid(packageId)) {
+    return NextResponse.json({ success: false, message: 'Invalid Package ID' }, { status: 400 });
+  }
+
+  try {
+    const packageData = await UpcommingTrips.findById(packageId);
+    if (!packageData) {
+      return NextResponse.json({ success: false, message: 'Package not found' }, { status: 404 });
+    }
+    return NextResponse.json({ success: true, data: packageData });
+  } catch (error) {
+    console.error('Error fetching package:', error); // Log the error for debugging
+    return NextResponse.json({ success: false, message: error.message }, { status: 400 });
+  }
+}
